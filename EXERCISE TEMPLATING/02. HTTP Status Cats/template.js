453b58d6f2013d15251e73c1ede8768f374eb37c
Erik <vfxbaliof@gmail.com>
2018-08-02 22:03:16 +0300

    02. HTTP Status Cats

--- EXERCISE TEMPLATING/02. HTTP Status Cats/template.js
+++ EXERCISE TEMPLATING/02. HTTP Status Cats/template.js
@@ -1,19 +1,17 @@
 $(() => {
     renderCatTemplate();
-    console.log('test o');
 
     function renderCatTemplate() {
-        // TODO: Render cat template and attach events
         $('.btn').on('click', function () {
-            if($(this).text() === 'Show status code'){
+            if ($(this).text() === 'Show status code') {
                 $(this).text('Hide status code');
                 let source = $('#cat-template').html();
-                let template  = Handlebars.compile(source);
+                let template = Handlebars.compile(source);
                 let id = $(this).parent('div').children('div').attr('id');
                 let catInfo = cats.filter(cat => cat.id === id);
                 let html = template(catInfo[0]);
                 $(this).parent().append(html);
-            }else{
+            } else {
                 $(this).text('Show status code');
                 console.log($(this).parent());
                 console.log($(this).parent().children('#info').remove());
@@ -22,5 +20,4 @@ $(() => {
 
         })
     }
-
 })
